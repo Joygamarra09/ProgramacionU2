6be5bbc0802417784713e7df68e0899edc8a5c73
Joygamarra09 <yoanagamarra09@gmail.com>
2025-03-30 02:50:36 -0300

    Agregando mi ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,4 +3,4 @@ nombre = "Joana"
 apellido = "Gamarra"
 edad = 24
 
-print(f"Hola! Tu nombre es {nombre}, tu apellido es {apellido} y tu edad es de {edad} años.")
+print(f"Hola! Tu nombre es {nombre}, tu apellido es {apellido} y tu edad es de {edad} años. Te damos la bienvenida.")
